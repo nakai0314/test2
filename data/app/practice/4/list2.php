25d5141395ab5e2d20cf60a8952692d61706b933
nakai0314 <amuteto.nakai@gmail.com>
2019-10-29 05:14:08 +0900

    変更

---
+++ data/app/practice/4/list2.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<style>
+ul{width:100px;}
+.color-red{background-color:red;}
+</style>
+</head>
+<body>
+    <ul>
+    <?php for ($i = 1; $i <= 20; $i++){
+        // ここに連続する処理の内容を書き込む
+    }?>
+        <li class="color-red">1</li>
+        <li>2</li>
+        <li class="color-red">3</li>
+        <li>4</li>
+    </ul>
+</body>
+</html>
