94a4aabd212fc4a4eb639433e0ce6c46a0425392
nakai0314 <amuteto.nakai@gmail.com>
2019-10-29 02:53:40 +0900

    変更

---
+++ data/app/practice/4/switch.php
@@ -0,0 +1,16 @@
+<?php
+$language =1;
+
+switch($language){
+    case 1:
+        echo 'こんにちは';
+        break;
+    case 2:
+        echo 'HELLO';
+        break;
+    case 3:
+        echo 'Bonjour';
+        break;
+    default:
+        echo '入力した数値が違っています。';
+}
